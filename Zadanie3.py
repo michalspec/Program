948cf137405e314e018676f19bb5410741849fa7
Michał Wróblewski <MasterWrobel@outlook.com>
2019-10-10 12:42:37 +0200

    Add zadanie3 file

---
+++ Zadanie3.py
@@ -0,0 +1,10 @@
+print('{:^30}'.format( 'testowy przyklad')) # center value
+print('testowy przyklad')
+
+print('{:>10}'.format('testowy'))  # align right
+
+print('{:10}'.format('test')) # alignleft
+
+print('{:_<10}'.format('test')) # padding character
+
+print('{:^6}'.format('zip'))
