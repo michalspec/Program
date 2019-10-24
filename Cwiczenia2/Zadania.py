549725dc263c71f672c9b976fe091c645c31dc5b
Michał Wróblewski <MasterWrobel@outlook.com>
2019-10-24 15:09:03 +0200

    Exercies 2.1-2.3 done

--- Cwiczenia2/Zadania.py
+++ Cwiczenia2/Zadania.py
@@ -9,3 +9,17 @@ def podzial(a_lista,b_lista):
             res.append(i)
 
     return res
+
+#zadanie2
+def slownik(data_text):
+    letters =[]
+    for i in range(0,len(data_text),1):
+        letters.append(data_text[i])
+
+    dict = {'length':len(data_text), 'letters': letters,'Big_letters': data_text.upper(),
+            'small_letters': data_text.lower()}
+    return dict
+#zadanie3
+def deleter(text,letter):
+    text = text.replace(letter,"")
+
